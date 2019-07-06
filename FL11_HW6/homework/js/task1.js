4855bad242ad897bbdccc68b5442303ba6fcca54
Nikita Sidorenko <nixid13@gmail.com>
2019-07-06 10:29:42 +0300

    Minor changes

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -4,12 +4,12 @@ let b1 = +prompt('Input value for b1(Xb)', '');
 let b2 = +prompt('Input value for b2(Yb)', '');
 let c1 = +prompt('Input value for c1(Xc)', '');
 let c2 = +prompt('Input value for c2(Yc)', '');
-const divider = 2;
+const DIVIDER = 2;
 
 if (a1 === b1 && a2 === b2) {
     console.log(false);
 } else {
-    console.log((a1 + b1) / divider === c1 && (a2 + b2) / divider === c2);
+    console.log((a1 + b1) / DIVIDER === c1 && (a2 + b2) / DIVIDER === c2);
 }
 
 
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -3,7 +3,7 @@ let b = +prompt('Enter B side of triangle', '');
 let c = +prompt('Enter C side of triangle', '');
 
 if (a > 0 && b > 0 && c > 0) {
-    if (a > b + c || b > a + c || c > a + b) {
+    if (a >= b + c || b >= a + c || c >= a + b) {
         console.log('Triangle doesn’t exist');
     } else if (a === b && b === c && c === a) {
         console.log('Equivalent triangle');
